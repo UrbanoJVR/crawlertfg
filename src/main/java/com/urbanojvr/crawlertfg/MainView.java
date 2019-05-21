6a006e9b6310c5588ae1a875dead43134f457975
urbanojvr <u.villanueva95@gmail.com>
2019-05-21 04:01:05 +0200

    POC finished

--- src/main/java/com/urbanojvr/crawlertfg/MainView.java
+++ src/main/java/com/urbanojvr/crawlertfg/MainView.java
@@ -18,11 +18,31 @@ public class MainView {
     }
 
     public static void main(String[] args){
+        setLookAndFeel();
         JFrame mainFrame = new JFrame("JSOUP CRAWLER");
         mainFrame.setContentPane(new MainView().contentPane);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setSize(1280, 720);
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setVisible(true);
+    }
+
+    public static void setLookAndFeel(){
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (UnsupportedLookAndFeelException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error loading L&F", JOptionPane.ERROR_MESSAGE);
+        }
+        catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error loading L&F", JOptionPane.ERROR_MESSAGE);
+        }
+        catch (InstantiationException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error loading L&F", JOptionPane.ERROR_MESSAGE);
+        }
+        catch (IllegalAccessException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error loading L&F", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
